f079a74c2405690674a9e30213c891f9ef314986
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2022-04-02 01:43:52 -0300

    Fix Startup Script

--- startup-script.sh
+++ startup-script.sh
@@ -5,13 +5,8 @@ set -v
 
 # Install or update needed software
 apt-get update
-apt-get install -yq git mysql-server supervisor python python-pip python3-distutils
-
-## MySQL Config
-export PASSWORD=`openssl rand -base64 32`; echo "Root password is : $PASSWORD"
-echo "ALTER USER 'root'@'localhost'
-      IDENTIFIED WITH mysql_native_password BY '$PASSWORD'" \
-      | sudo mysql -u root
+apt-get install -yq git wget supervisor python python-pip python3-distutils
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
 
 ## App Deploy
 pip install --upgrade pip virtualenv
@@ -34,9 +29,21 @@ chown -R pythonapp:pythonapp /opt/app
 # Put supervisor configuration in proper place
 cp /opt/app/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
+## MySQL Config
+dpkg -i mysql-apt-config_0.8.22-1_all.deb
+apt-get update
+apt-get install -yq git mysql-server
+
+export PASSWORD=`openssl rand -base64 32`; echo "Root password is : $PASSWORD"
+echo "ALTER USER 'root'@'localhost'
+      IDENTIFIED WITH mysql_native_password BY '$PASSWORD'" \
+      | sudo mysql -u root
+
 mysql -h 127.0.0.1 -P 3306 -u root -p$PASSWORD -e "CREATE USER 'playuser'@'%' IDENTIFIED BY '123456';"
 mysql -h 127.0.0.1 -P 3306 -u root -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'playuser'@'%' WITH GRANT OPTION;"
-mysql -h 127.0.0.1 -P 3306 -u playuser -p123456 < Playlist.sql 
+mysql -h 127.0.0.1 -P 3306 -u playuser -p123456 < /opt/app/Playlist.sql 
+
+export CLOUD_SQL_USERNAME=playuser CLOUD_SQL_PASSWORD=123456 CLOUD_SQL_DATABASE_NAME=playlist DB_LOCAL_HOST=localhost
 
 # Start service via supervisorctl
 supervisorctl reread
